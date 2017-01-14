c530c0bbafb9dc4f65ca0d0e4dc21267e6523d90
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-01-13 21:31:30 -0500

    Import controllers from PageView template.

---
+++ LibraryApp_Firebase/BookCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookCollectionViewCell.swift
+//  LibraryApp_Firebase
+//
+//  Created by Anthony Whitaker on 1/13/17.
+//  Copyright © 2017 Anthony Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+    
+}
